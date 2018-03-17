a88d36b26a422fcf4cedf2ef9dc72a6212d4ee37
Brennan Heisler <learn@heislercreative.com>
2018-03-17 21:16:17 +0000

    automatically backed up by learn

--- models/student.rb
+++ models/student.rb
@@ -1,16 +1,15 @@
-    class Student
-      attr_reader :name, :grade
+class Student
+  attr_reader :name, :grade
+  STUDENTS = []
      
-     STUDENTS = []
+  def initialize(params)
+    @name = params[:name]
+    @grade = params[:grade]
+    STUDENTS << self
+  end
      
-      def initialize(params)
-        @name = params[:name]
-        @grade = params[:grade]
-        STUDENTS << self
-      end
+  def self.all
+    STUDENTS
+  end
      
-      def self.all
-        STUDENTS
-      end
-     
-    end
+end
