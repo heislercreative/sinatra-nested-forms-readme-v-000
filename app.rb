ecbea97eb8638698ba00a198c7bebd0fd29794ad
Brennan Heisler <learn@heislercreative.com>
2018-03-17 21:40:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/course'
+require_relative 'models/student'
 
 class App < Sinatra::Base
 
   get '/' do
     erb :new
+  end
+  
+  post '/student' do
+    @student = Student.new(params[])
   end
 
 end
