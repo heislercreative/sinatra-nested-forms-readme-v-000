bf9da5a5c1a7b108eac6b3dae6ec8ba20ae81917
Brennan Heisler <learn@heislercreative.com>
2018-03-17 21:41:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,11 @@ class App < Sinatra::Base
   end
   
   post '/student' do
-    @student = Student.new(params[])
+    @student = Student.new(params[:student])
+    
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
   end
 
 end
